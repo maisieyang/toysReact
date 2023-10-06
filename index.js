14d063435c31b4ba5a631991af1826e664d17e55
yangxiyue <yangxiyuebupt@gmail.com>
2023-10-06 22:35:21 +0800

    feat: more

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 "use strict";
 
-var element = /*#__PURE__*/React.createElement("h1", null, "Hello, world!");
+var _jsx = require("../lib/jsx");
+var _render = require("../lib/dom/render");
+var element = (0, _jsx.createElement)("div", {
+  className: "app"
+}, (0, _jsx.createElement)("h1", null, "Hello World!"), (0, _jsx.createElement)("p", null, "This is our custom React."));
+var container = document.getElementById('root');
+(0, _render.render)(element, container);
 console.log(element);
--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,16 @@
-const element = <h1>Hello, world!</h1>;
-console.log(element);
+import { createElement } from "../lib/jsx";
+import { render } from "../lib/dom";
+
+
+const element = (
+    <div className="app">
+      <h1>Hello World!</h1>
+      <p>This is our custom React.</p>
+    </div>
+  );
+
+const container = document.getElementById('root');
+render(element, container);
+
+  
+// console.log(element);
